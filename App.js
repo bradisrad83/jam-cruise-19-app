bdb8cc5a73f3dba7530ed0423b4ffd11e02bf326
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2022-05-09 19:59:56 -0400

    first commit need to start somewhere

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import HomePage from "./assets/pages/HomePage";
+
+const App = () => {
+  return (
+      <HomePage />
+  );
+};
+
+export default App;
---
+++ assets/pages/HomePage.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import {Image, View, Text, StyleSheet} from 'react-native';
+
+const image = require('../images/jc19.jpeg');
+
+const calculateTimeLeft = () => {
+    const difference = +new Date('02/06/2023 03:00:00PM') - +new Date();
+    let timeLeft = {
+        days: 0, 
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    };
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+    }
+    return timeLeft;
+}
+
+const HomePage = () => {
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+        return () => clearTimeout(timer);
+    });
+    return (
+        <View style={styles.page}>
+            <Image style={styles.image} source={image} />
+            <View style={styles.time}>
+                <Text style={styles.text}>{timeLeft.days+' Days'}</Text>
+                <Text style={styles.text}>{timeLeft.hours+' Hours'}</Text>
+                <Text style={styles.text}>{timeLeft.minutes+' Minutes'}</Text>
+                <Text style={styles.text}>{timeLeft.seconds+' Seconds'}</Text>
+                <Text style={styles.text}>UNTIL WE SET SAIL!!!</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    page: {
+        paddingTop: 50,
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        flex: 1,
+    },
+    image: {
+        height: 250,
+        width: 320,
+    },
+    time: {
+        marginTop: 20,
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 25,
+        marginTop: 10,
+        color: '#169eba',
+        fontWeight: 'bold',
+    }
+  });
+
+  export default HomePage;
