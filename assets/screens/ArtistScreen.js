8d27cfa6e1bc73bbc1b977e839abf3fb1ff7289b
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2022-05-15 08:09:15 -0400

    gonna play with the new M1

--- assets/screens/ArtistScreen.js
+++ assets/screens/ArtistScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView, Image, Dimensions, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, ScrollView, Image, Dimensions, TouchableOpacity, FlatList} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const windowWidth = Dimensions.get('window').width;
 
@@ -8,6 +8,16 @@ MaterialIcons.loadFont();
 const ArtistScreen = (props) => {
     const navigation = props.navigation;
     let artist = props.route.params.params.artist;
+    const renderSchedule = ({schedule}) => {
+        <View stlyle={styles.timeSlot}>
+            <Text style={styles.timeSlotText}>Date</Text>
+            <Text style={styles.timeSlotText}>Time</Text>
+            <Text style={styles.timeSlotText}>Stage</Text>
+            <TouchableOpacity style={styles.timeSlotButton}>
+                <Text>Add Reminder</Text>
+            </TouchableOpacity>
+        </View>
+    };
     return (
             <View style={styles.page}>
                 <TouchableOpacity style={styles.navbar} onPress={() => navigation.goBack()}>
@@ -15,6 +25,9 @@ const ArtistScreen = (props) => {
                 </TouchableOpacity>
                 <ScrollView styles={styles.artist}>
                     <Image source={artist.imageUrl} style={styles.image}/>
+                    <Text style={styles.text}>{artist.name}</Text>
+
+                    <Text style={styles.paragraph}>{artist.about}</Text>
                 </ScrollView>
             </View>
     )
@@ -37,6 +50,11 @@ const styles = StyleSheet.create({
         color: '#169eba',
         fontWeight: 'bold',
     },
+    paragraph: {
+        fontSize: 16,
+        marginTop: 20,
+        color: '#169eba',
+    },  
     navbar: {
         alignSelf: 'flex-start',
         marginTop: -50,
@@ -46,12 +64,34 @@ const styles = StyleSheet.create({
     },
     image: {
         margin: 'auto',
-        width: windowWidth * .8,
-        height:200,
-        resizeMode: 'contain',
+        width: windowWidth,
+        height:210,
+        resizeMode: 'cover',
+        borderRadius: 100/2,
+        borderColor: '#169eba',
+        borderWidth: 2,
     },
     artist: {
         marginTop: 75,
+    },
+    timeSlot: {
+        flexDirection: 'row',
+        justfityContent: 'flex-start',
+    },
+    timeSlotText: {
+        marginLeft: 6,
+        color: '#169eba',
+        fontSize: 12,
+        fontWeight: 'bold',
+    },
+    timeSlotButton: {
+        backgroundColor: '#169eba',
+        color: '#000000',
+        paddingTop: 6,
+        paddingBottom: 6,
+        paddingRight: 10,
+        paddingLeft: 10,
+        borderRadius: 14,
     }
   });
 
